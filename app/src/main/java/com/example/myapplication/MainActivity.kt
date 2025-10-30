9076bfac94611acb1bcf455f619ad87bc57aaab5
valentina1-pixel <valenntna887@gmail.com>
2025-10-30 15:09:12 +0700

    feat: implement activity and intent navigation.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,35 +9,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Log.d("MainActivity", "Aplikasi berhasil Dijalankan!")
+
+        Log.d("MainActivity", "Aplikasi berhasil dijalankan!")
 
         val etUsername: EditText = findViewById(R.id.et_username)
         val btnDaftar: Button = findViewById(R.id.btn_daftar)
         val btnLogin: Button = findViewById(R.id.btn_login)
 
+        // Tombol DAFTAR → pindah ke RegisterActivity
         btnDaftar.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Tombol LOGIN → pindah ke HomeActivity (dengan kirim username)
+        btnLogin.setOnClickListener {
             val username = etUsername.text.toString()
             val intent = Intent(this, HomeActivity::class.java)
-
             intent.putExtra("EXTRA_USERNAME", username)
             startActivity(intent)
-
-            btnDaftar.setOnClickListener {
-                val intent = Intent(this, RegisterActivity::class.java)
-                startActivity(intent)
-            }
         }
     }
 }
